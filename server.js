691e8d8455301195478d5e1ab0daf2d54ec4eb03
siddharth <sidhu2006be@gmail.com>
2025-11-08 16:34:52 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Wallpaper App Backend Running ✅");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
